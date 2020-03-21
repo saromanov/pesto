258f43b101e72dec793149534d3347440bc5fdf8
Sergey <xxsmotur@gmail.com>
2020-03-21 20:12:22 +0500

    Add configurations

--- pesto/app.py
+++ pesto/app.py
@@ -2,6 +2,33 @@ import logging
 
 from flask import Flask, render_template
 
+
+def make_app(config=None):
+    app = Flask(__name__)
+
+def configure(app:Flask):
+     app.config.from_object("pesto.configs.default.DefaultConfig")
+     config = get_config(app, config)
+     app.config["CONFIG_PATH"] = config
+     app_config_from_env(app, prefix="PESTO_")
+
+     configure_logging(app)
+
+     deprecation_level = app.config.get("DEPRECATION_LEVEL", "default")
+
+
+def configure_celery_app(app, celery):
+    """Configures the celery app."""
+    app.config.update({"BROKER_URL": app.config["CELERY_BROKER_URL"]})
+    celery.conf.update(app.config)
+
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
