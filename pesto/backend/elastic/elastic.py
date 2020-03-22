54551fcc5f3ea5dc37baba851c3e0fbfe1c6b848
Sergey <xxsmotur@gmail.com>
2020-03-22 18:11:08 +0500

    Add method for adding index to elastic search

--- pesto/backend/elastic/elastic.py
+++ pesto/backend/elastic/elastic.py
@@ -1,9 +1,13 @@
 import datetime
+import uuid
+
 from elasticsearch7 import Elasticsearch
 
 def init(config):
     es = Elasticsearch(config['ELASTIC_ADDRESS'])
-    #es.index(index='article-text', id=1, body = {'text': 'simple', 'datetime': datetime.datetime.now()})
+
+def add_to_index(es, index, body):
+    es.index(index=index, id=uuid.uuid4(), body = {'text': 'simple', 'datetime': datetime.datetime.now()})
 
 def remove_from_index(app, index, model):
     app.elasticsearch.delete(index=index, id=model.id)
