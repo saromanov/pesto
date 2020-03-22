23d0897f9cbc8c421c04e12b87e929c86bcfa9fd
Sergey <xxsmotur@gmail.com>
2020-03-22 14:07:40 +0500

    Init configuration for db

--- pesto/backend/db/db.py
+++ pesto/backend/db/db.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def query(cls, **kw):
+    q = db.session.query(cls)
+
+    if kw:
+        q = q.filter_by(**kw)
+
+    return q
+
+def get(cls, id):
+    return cls.query().get(id)
