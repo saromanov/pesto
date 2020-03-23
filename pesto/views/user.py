e62807c7b5ae9144acdd9b275b890367c6a1e0f3
Sergey <xxsmotur@gmail.com>
2020-03-23 23:54:41 +0500

    Update template and check if user is exist

--- pesto/views/user.py
+++ pesto/views/user.py
@@ -1,16 +1,31 @@
 from flask.views import MethodView
-from flask import render_template, Blueprint, Flask, redirect
+from flask import render_template, Blueprint, Flask, redirect, flash
 from pluggy import HookimplMarker
+from flask_login import UserMixin
 
 from model import User
 from utils import register_view
 from forms import RegisterForm
+from backend.db import db
 
 impl = HookimplMarker("pesto")
 
 class UserProfile(MethodView):
     def get(self, id):
         return render_template('user.html', form=form, user=user)
+
+class UserLogin(UserMixin, MethodView):
+    def post(self):
+        form = LoginForm()
+        if form.validate_on_sublit():
+            email = form.get('email')
+            first_name = form.get('first_name')
+            last_name = form.get('last_name')
+            password = form.get('password')
+            exist = User.query.filter_by(email=email).first()
+            if exist:
+                flash('User ealready exisr')
+                return redirect('/')
 
 class UserRegister(MethodView):
     def get(self):
@@ -19,6 +34,8 @@ class UserRegister(MethodView):
     def post(self):
         form = RegisterForm()
         if form.validate_on_submit():
+            db.session.add(form)
+            db.session.commit()
             return redirect('/')
         return "Error"
     
@@ -32,5 +49,6 @@ def make_blueprints_user(app:Flask):
     '''
     bp = Blueprint('user', __name__)
     register_view(bp, routes=['/users/profile'], view_func=UserProfile.as_view("user"))
+    register_view(bp, routes=['/users/login'], view_func=UserProfile.as_view("login"))
     register_view(bp, routes=['/users/register'], view_func=UserRegister.as_view("register"))
     app.register_blueprint(bp)
