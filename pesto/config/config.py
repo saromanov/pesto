0ed9a095c706d6a4abd76895f823c5fef9d6281b
Sergey <xxsmotur@gmail.com>
2020-03-25 17:41:11 +0500

    Set up of postgres connection

--- pesto/config/config.py
+++ pesto/config/config.py
@@ -5,7 +5,11 @@ class Config:
     ELASTIC_ADDRESS = 'http://localhost:9200'
     SECRET_KEY = os.urandom(32)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    POSTGRES_USER = 'pesto'
+    POSTGRES_URL = '127.0.0.1:5432'
+    POSTGRES_DB = 'pesto'
+    POSTGRES_PASSWORD = 'pesto'
+    SQLALCHEMY_DATABASE_URI = DB_URL = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_URL}/{POSTGRES_DB}'
 
 class DevConfig(Config):
     DEBUG = True
