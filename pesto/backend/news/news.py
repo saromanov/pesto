02cc255b66b9189cbd3fec1739f6f414f5ed80dd
Sergey <xxsmotur@gmail.com>
2020-03-28 18:30:44 +0500

    Add method for return hot topics

--- pesto/backend/news/news.py
+++ pesto/backend/news/news.py
@@ -1,4 +1,5 @@
-from newspaper import Article
+from typing import List
+from newspaper import Article, hot
 
 def get_articles_urls(url:str):
     if not url:
@@ -6,3 +7,6 @@ def get_articles_urls(url:str):
     papers = newspaper.build(url)
     for p in papers:
         yield p.url
+
+def hot_topics() -> List[str]:
+    return hot()
