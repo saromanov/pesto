00ea762d4aa255cc524e48ed05f7f84086681328
Sergey <xxsmotur@gmail.com>
2020-04-01 01:25:38 +0500

    Adding hot topics to cache

--- pesto/backend/celery/tasks.py
+++ pesto/backend/celery/tasks.py
@@ -1,6 +1,8 @@
+import datetime
 from .celery import celery 
-
+from backend.news import hot_topics
+from backend.cache import sadd
 
 @celery.task(bind=True)
 def store_hot_topics(a):
-    print('YESSSSSSS')
+    sadd(datetime.datetime.now().strftime('hot-topics:%Y:%m:%d:%H'), ','.join(hot_topics()))
